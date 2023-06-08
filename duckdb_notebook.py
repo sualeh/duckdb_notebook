401072f5e676cae199e92d2ea6bd4394fbd77079
Sualeh Fatehi <sualeh@hotmail.com>
2023-06-08 19:45:08 -0400

    Load two tables, and join them

--- duckdb_notebook.py
+++ duckdb_notebook.py
@@ -4,49 +4,80 @@ import duckdb
 conn = duckdb.connect()
 print(f"DuckDB connection {conn}")
 
-df = conn.execute("""
-    SELECT 
-      *
-    FROM   
-      read_csv_auto('datasets/people.csv', header=True)
-""").df()
 # %%
 
-conn.register("people", df)
+conn.register(
+    "people",
+    conn.execute(
+        """
+    SELECT
+      *
+    FROM
+      read_csv_auto('datasets/people.csv', header=True)
+"""
+    ).df(),
+)
+
 conn.execute("DESCRIBE people").df()
 
 # %%
-conn.execute("FROM people").df()
+
+conn.register(
+    "people_personal",
+    conn.execute(
+        """
+    SELECT
+      *
+    FROM
+      read_csv_auto('datasets/people_personal.csv', header=True)
+"""
+    ).df(),
+)
+
+conn.execute("DESCRIBE people_personal").df()
 
 # %%
-conn.execute("SELECT COUNT(*) AS people_count FROM people").df()
+
+conn.execute("FROM people LIMIT 5").df()
 
 # %%
-conn.execute("""
-  SELECT 
-    substr(FirstName, 1, 1)
-      AS initial,
-    COUNT(*) 
-      AS total_people 
-  FROM 
-    people 
-  GROUP BY
-    substr(FirstName, 1, 1)
-""").df()
+
+conn.execute("FROM people_personal LIMIT 5").df()
+
 # %%
-conn.execute("""
-CREATE OR REPLACE TABLE initials 
-AS 
-  SELECT 
-    substr(FirstName, 1, 1)
-      AS initial,
-    COUNT(*) 
-      AS total_people 
-  FROM 
-    people 
-  GROUP BY
-    substr(FirstName, 1, 1)
-""")
-             
-conn.execute("FROM initials").df()
+
+conn.execute(
+    """
+  SELECT
+    people.Id,
+    people.FirstName,
+    people.LastName,
+    people_personal.FavoriteColor
+  FROM
+    people
+    INNER JOIN people_personal
+      ON people.Id = people_personal.Id
+"""
+).df()
+
+# %%
+
+conn.execute(
+    """
+  CREATE OR REPLACE TABLE initials
+  AS
+    SELECT
+      substr(FirstName, 1, 1)
+        AS Initial,
+      COUNT(*)
+        AS TotalPeople
+    FROM
+      people
+    GROUP BY
+      substr(FirstName, 1, 1)
+"""
+)
+
+conn.execute("FROM initials ORDER BY Initial").df()
+
 # %%
